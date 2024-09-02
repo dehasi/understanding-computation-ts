0749dc7f50bb738c7a6f526cb80970c62f955031
Ravil Galeyev <dehasi@yandex.ru>
2024-09-02 19:58:43 +0200

    x

--- tests/warmup/expr.test.ts
+++ tests/warmup/expr.test.ts
@@ -1,9 +1,12 @@
 import { expr } from "../../src/warmup/expr";
 
-describe('Test', () => {
+describe('Test simple expressions', () => {
     let expressions: Map<string, number> = new Map([
         ['0', 0],
         ['2+3', 5],
+        ['2+2*2', 6],
+        ['2+4/2', 4],
+        ['2+5%2', 3],
         ['2-3', -1]
     ]);
 
