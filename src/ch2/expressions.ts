80fbbfbe0a740b0a368695b9496fc10aeff4de2b
Ravil Galeyev <dehasi@proton.me>
2024-09-06 21:36:15 +0200

    CH-2

--- src/ch2/expressions.ts
+++ src/ch2/expressions.ts
@@ -1,16 +1,16 @@
-class Struct {
+class Expression {
   // readonly reduceable: () => boolean;
 
   reducible(): boolean {
     throw new Error("Method must be implemented.");
   }
 
-  reduce(): Struct {
+  reduce(): Expression {
     throw new Error("Method must be implemented.");
   }
 }
 
-class Nmbr extends Struct {
+class Nmbr extends Expression {
   readonly value: number;
 
   constructor(value: number) {
@@ -27,17 +27,17 @@ class Nmbr extends Struct {
   }
 }
 
-class Add extends Struct {
-  readonly left: Struct;
-  readonly right: Struct;
+class Add extends Expression {
+  private readonly left: Expression;
+  private readonly right: Expression;
 
-  constructor(left: Struct, right: Struct) {
+  constructor(left: Expression, right: Expression) {
     super();
     this.left = left;
     this.right = right;
   }
 
-  reduce(): Struct {
+  reduce(): Expression {
     if (this.left.reducible()) {
       return new Add(this.left.reduce(), this.right);
     } else if (this.right.reducible()) {
@@ -55,23 +55,23 @@ class Add extends Struct {
   }
 }
 
-class Multiply extends Struct {
-  readonly left: Struct;
-  readonly right: Struct;
+class Multiply extends Expression {
+  private readonly left: Expression;
+  private readonly right: Expression;
 
-  constructor(left: Struct, right: Struct) {
+  constructor(left: Expression, right: Expression) {
     super();
     this.left = left;
     this.right = right;
   }
 
-  reduce(): Struct {
+  reduce(): Expression {
     if (this.left.reducible()) {
       return new Multiply(this.left.reduce(), this.right);
     } else if (this.right.reducible()) {
       return new Multiply(this.left, this.right.reduce());
     } else {
-      return new Nmbr((this.left as Nmbr).value + (this.right as Nmbr).value);
+      return new Nmbr((this.left as Nmbr).value * (this.right as Nmbr).value);
     }
   }
   reducible(): boolean {
@@ -82,4 +82,26 @@ class Multiply extends Struct {
   }
 }
 
-export { Struct, Nmbr, Add, Multiply };
+
+class Machine {
+  private expression: Expression
+  constructor(expression: Expression) {
+    this.expression = expression;
+  }
+
+  step() :Expression {
+    return this.expression = this.expression.reduce()
+  }
+
+  run(): Array<Expression> {
+    const result = new Array<Expression>();
+
+    while(this.expression.reducible()){
+      result.push(this.expression)
+      this.step()
+    }
+    result.push(this.expression)
+    return result
+  }
+}
+export { Expression, Nmbr, Add, Multiply, Machine };
--- tests/ch2/expressions.test.ts
+++ tests/ch2/expressions.test.ts
@@ -1,14 +1,27 @@
-import { Struct, Add, Multiply, Nmbr } from "../../src/ch2/expressions";
+import { Add, Machine, Multiply, Nmbr } from "../../src/ch2/expressions";
 
 describe("Reduction", () => {
-  test("name", () => {
+  test("toString", () => {
     const expr = new Multiply(
       new Nmbr(1),
-      new Multiply(
-        new Add(new Nmbr(2), new Nmbr(3)),
-        new Nmbr(4)),
+      new Multiply(new Add(new Nmbr(2), new Nmbr(3)), new Nmbr(4)),
     );
 
-    expect(expr.toString()).toEqual("1 * 2 + 3 * 4")
+    expect(expr.toString()).toEqual("1 * 2 + 3 * 4");
+  });
+});
+
+describe("Reduction", () => {
+  test("Machine", () => {
+    const machine = new Machine(
+      new Add(
+        new Multiply(new Nmbr(1), new Nmbr(2)),
+        new Multiply(new Nmbr(3), new Nmbr(4)),
+      ),
+    );
+
+    const reductions = machine.run().map((x) => x.toString());
+
+    expect(reductions).toEqual(["1 * 2 + 3 * 4", "2 + 3 * 4", "2 + 12", "14"]);
   });
 });
