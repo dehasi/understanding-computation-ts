59de584cc0e396f214e9c27b8baf38dff1ae397f
Ravil Galeyev <dehasi@proton.me>
2024-09-08 12:34:07 +0200

    big step

--- src/ch2/big-step/statements.ts
+++ src/ch2/big-step/statements.ts
@@ -111,51 +111,6 @@ class Sequence extends Statement {
   }
 }
 
-// This is my solution. the Books solution is below
-class MyWhile extends Statement {
-  private readonly condition: Expression;
-  private readonly body: Statement;
-  private readonly prev?: MyWhile;
-
-  constructor(
-    condition: Expression,
-    body: Statement,
-    prev: MyWhile | undefined = undefined,
-  ) {
-    super();
-    this.condition = condition;
-    this.body = body;
-    this.prev = prev;
-  }
-
-  reduce(env: Environment): [Statement, Environment] {
-    if (this.condition.reducible()) {
-      return [new MyWhile(this.condition.reduce(env), this.body, this), env];
-    }
-
-    if (TRUE.equals(this.condition)) {
-      if (this.body.reducible()) {
-        const [reduced_body, reduced_env] = this.body.reduce(env);
-        return [new MyWhile(this.condition, reduced_body, this), reduced_env];
-      }
-      // while is finished
-      let p: MyWhile | undefined = this;
-      while (p.prev) p = p.prev;
-      return [p, env];
-    } else if (FALSE.equals(this.condition)) {
-      return [DO_NOTHING, env];
-    } else {
-      throw new Error("Expected Boolean, got " + typeof this.condition);
-    }
-  }
-  reducible(): boolean {
-    return true;
-  }
-  toString(): string {
-    return `while(${this.condition}) {${this.body}}`;
-  }
-}
-
 class While extends Statement {
   private readonly condition: Expression;
   private readonly body: Statement;
