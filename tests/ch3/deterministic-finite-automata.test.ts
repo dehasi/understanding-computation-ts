ede3987b4ba5c00bf8bb7e8696e2faab21deda8a
Ravil Galeyev <dehasi@proton.me>
2024-09-19 20:16:29 +0200

    x

--- tests/ch3/deterministic-finite-automata.test.ts
+++ tests/ch3/deterministic-finite-automata.test.ts
@@ -1,4 +1,4 @@
-import { DFARulebook, FARule } from "../../src/ch3/deterministic-finite-automata"
+import { DFA, DFARulebook, FARule } from "../../src/ch3/deterministic-finite-automata"
 
 describe('DFA', () => {
     const rulebook = new DFARulebook([
@@ -10,5 +10,26 @@ describe('DFA', () => {
         expect(rulebook.next_state(1, 'a')).toEqual(2)
         expect(rulebook.next_state(1, 'b')).toEqual(1)
         expect(rulebook.next_state(2, 'b')).toEqual(3)
+    });
+
+    test('accepting', () => {
+        expect(new DFA(1, [1, 3], rulebook).accepting()).toEqual(true);
+        expect(new DFA(1, [3], rulebook).accepting()).toEqual(false);
+    })
+    
+    test('accepting', () => {
+        const dfa = new DFA(1, [3], rulebook);
+        expect(dfa.accepting()).toEqual(false);
+        
+        dfa.read_charactter('b');
+        expect(dfa.accepting()).toEqual(false);
+
+        dfa.read_charactter('a');
+        dfa.read_charactter('a');
+        dfa.read_charactter('a');
+        expect(dfa.accepting()).toEqual(false);
+
+        dfa.read_charactter('b');
+        expect(dfa.accepting()).toEqual(true);
     })
 })
