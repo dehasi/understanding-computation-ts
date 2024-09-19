bfbd47099c2382b0018d1238be654a55772e16de
Ravil Galeyev <dehasi@proton.me>
2024-09-19 20:50:35 +0200

    x

--- tests/ch3/nondeterministic-finite-automata.test.ts
+++ tests/ch3/nondeterministic-finite-automata.test.ts
@@ -4,12 +4,16 @@ import { set } from "./test-utils"
 
 describe('NFA', () => {
     const rulebook = new NFARulebook([
-        new FARule(1, 'a', 2), new FARule(1, 'b', 1), new FARule(1, 'b', 2),
+        new FARule(1, 'a', 1), new FARule(1, 'b', 1), new FARule(1, 'b', 2),
         new FARule(2, 'a', 3), new FARule(2, 'b', 3),
         new FARule(3, 'a', 4), new FARule(3, 'b', 4),
     ])
 
     test('rulebook', () => {
-        expect(rulebook.next_states(set(1), 'b')).toEqual(set(1, 2))
+        expect(rulebook.next_states(set(1), 'b')).toEqual(set(1, 2));
+
+        expect(rulebook.next_states(set(1, 2), 'a')).toEqual(set(1, 3));
+
+        expect(rulebook.next_states(set(1, 3), 'b')).toEqual(set(1, 2, 4));
     })
 })
