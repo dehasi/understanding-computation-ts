52421b78f46f7dee5aacc7a41a5e5265ef03d11e
Ravil Galeyev <dehasi@proton.me>
2024-09-19 20:05:32 +0200

    xx

--- src/ch3/deterministic-finite-automata.ts
+++ src/ch3/deterministic-finite-automata.ts
@@ -13,20 +13,24 @@ function requred<T>(value: T | undefined, message?: string): T {
 }
 
 export type state = number;
-
+export type character = string
+// 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' |
+// 'l' | 'm' | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' |
+// 'w' | 'x' | 'y' | 'z';
 
 export class FARule {
     state: state;
-    character: string;
+    character: character;
     next_state: state;
 
-    constructor(state: state, character: string, next_state: state) {
+    constructor(state: state, character: character, next_state: state) {
+        assert(character.length === 1, `expected only one char, but found ${character}`);
         this.state = state;
-        this.character = character.charAt(0);
+        this.character = character;
         this.next_state = next_state;
     }
 
-    applies_to(state: state, character: string): boolean {
+    applies_to(state: state, character: character): boolean {
         return this.state === state && this.character == character
     }
 
@@ -46,14 +50,40 @@ export class DFARulebook {
         this.rules = rules;
     }
 
-    next_state(state: state, character: string): state {
+    next_state(state: state, character: character): state {
         return this.rule_for(state, character).follow();
     }
 
-    rule_for(state: state, character: string): FARule {
+    rule_for(state: state, character: character): FARule {
         return requred(
             this.rules.find(rule => rule.applies_to(state, character)),
             `FARule not found for (${state}, ${character})`
         );
+    }
+}
+
+export class DFA {
+    current_state: state;
+    accept_states: ReadonlyArray<state>;
+    rulebook: DFARulebook;
+
+    constructor(current_state: state, accept_states: ReadonlyArray<state>, rulebook: DFARulebook) {
+        this.current_state = current_state;
+        this.accept_states = accept_states;
+        this.rulebook = rulebook;
+    }
+
+    accepting(): boolean {
+        return this.accept_states.includes(this.current_state);
+    }
+
+    read_charactter(character: character): void {
+        this.current_state = this.rulebook.next_state(this.current_state, character);
+    }
+
+    read_string(string: string): void {
+        for (const ch of string) {
+            this.read_charactter(ch)
+        }
     }
 }
