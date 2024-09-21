94a664ce300a032bac4a5cf4d8264052f10085ab
Ravil Galeyev <dehasi@proton.me>
2024-09-21 16:13:21 +0200

    x

---
+++ tests/ch3/regular-expressions.test.ts
@@ -0,0 +1,19 @@
+import { Empty, Literal } from "../../src/ch3/regular-expressions"
+
+describe('Regular Expressions', () => {
+
+    test('Empty', () => {
+        const nfa_design = new Empty().to_nfa_design()
+
+        expect(nfa_design.accepts('')).toEqual(true);
+        expect(nfa_design.accepts('a')).toEqual(false);
+    })
+
+    test('Literal', () => {
+        const nfa_design = new Literal('a').to_nfa_design()
+
+        expect(nfa_design.accepts('')).toEqual(false);
+        expect(nfa_design.accepts('a')).toEqual(true);
+        expect(nfa_design.accepts('b')).toEqual(false);
+    })
+})
