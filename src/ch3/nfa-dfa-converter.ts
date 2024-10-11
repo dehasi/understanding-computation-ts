95c44de90f082cf2c3a559bf9f4bedd66c9ebc1c
Ravil Galeyev <dehasi@proton.me>
2024-10-11 21:30:54 +0200

    x

--- src/ch3/nfa-dfa-converter.ts
+++ src/ch3/nfa-dfa-converter.ts
@@ -131,6 +131,74 @@ export class NFADesign {
     }
 }
 
+
+export class DFARulebook {
+    private rules: ReadonlyArray<FARule>
+
+    constructor(rules: ReadonlyArray<FARule>) {
+        this.rules = rules;
+    }
+
+    next_state(state: state, character: character): state {
+        return this.rule_for(state, character).follow();
+    }
+
+    rule_for(state: state, character: character): FARule {
+        return required(
+            this.rules.find(rule => rule.applies_to(state, character)),
+            `FARule not found for (${state}, ${character})`
+        );
+    }
+}
+
+export class DFA {
+    current_state: state;
+    accept_states: ReadonlyArray<state>;
+    rulebook: DFARulebook;
+
+    constructor(current_state: state, accept_states: ReadonlyArray<state>, rulebook: DFARulebook) {
+        this.current_state = current_state;
+        this.accept_states = accept_states;
+        this.rulebook = rulebook;
+    }
+
+    accepting(): boolean {
+        return this.accept_states.includes(this.current_state);
+    }
+
+    read_character(character: character): void {
+        this.current_state = this.rulebook.next_state(this.current_state, character);
+    }
+
+    read_string(string: string): void {
+        for (const ch of string) {
+            this.read_character(ch)
+        }
+    }
+}
+
+export class DFADesign {
+    start_state: state;
+    accept_states: ReadonlyArray<state>;
+    rulebook: DFARulebook;
+
+    constructor(start_state: state, accept_states: ReadonlyArray<state>, rulebook: DFARulebook) {
+        this.start_state = start_state;
+        this.accept_states = accept_states;
+        this.rulebook = rulebook;
+    }
+
+    to_dfa(): DFA {
+        return new DFA(this.start_state, this.accept_states, this.rulebook);
+    }
+
+    accepts(string: string): boolean {
+        const dfa = this.to_dfa();
+        dfa.read_string(string)
+        return dfa.accepting();
+    }
+}
+
 export class NFASimulation {
     private nfa_design: NFADesign
 
@@ -145,7 +213,25 @@ export class NFASimulation {
     }
 
     rules_for(state: Set<state>): Set<FARule> {
-        return new Set([...this.nfa_design.rulebook.alphabet()].map(characheter => new FARule(state, characheter, this.next_state(state, characheter))))
+        return new Set([...this.nfa_design.rulebook.alphabet()].map(characheter =>
+            new FARule(state, characheter, this.next_state(state, characheter))))
 
+    }
+
+    discover_states_and_rules(states: Set<state>): [Set<state>, Set<FARule>] {
+        const rules = [...states].flatMap(state => [...this.rules_for(state)]).flat();
+        const more_states = new Set(rules.map(rule => rule.follow()))
+        if (is_subset(more_states, states))
+            return [states, new Set(rules)]
+        else
+            return [more_states, new Set(rules)]
+    }
+
+    to_dfa_design(): DFADesign {
+        const start_state = this.nfa_design.to_nfa()._current_states();
+        const [states, rules] = this.discover_states_and_rules(start_state);
+        const accept_states = [...states].filter(state => this.nfa_design.to_nfa(state).accepting());
+
+        return new DFADesign(start_state, accept_states, new DFARulebook([...rules]))
     }
 }
